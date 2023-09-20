2774f8f9acaeaab1b8e2171a7a192cad7c050f48
Rik <ambrth@gmail.com>
2023-09-20 22:36:43 +0200

    Update action.js

--- src/action.js
+++ src/action.js
@@ -8,7 +8,7 @@ const mountNewDisqus = ({ identifier, url }) => {
 
     const disqus = document.createElement('script');
     disqus.id = 'disqus-app-block';
-    disqus.src = 'https://svelte-test.disqus.com/embed.js';
+    disqus.src = '//discutest.disqus.com/embed.js';
     disqus.setAttribute('data-timestamp', +new Date());
     (document.head || document.body).appendChild(disqus);
 
